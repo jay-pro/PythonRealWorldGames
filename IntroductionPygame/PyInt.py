8d627afdcbf11a950725111e251eefe0918450db
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-21 22:50:11 +0700

    Naming and Filling the Game Screen

--- IntroductionPygame/PyInt.py
+++ IntroductionPygame/PyInt.py
@@ -2,6 +2,7 @@ import pygame
 width, height = 800, 600
 
 GScreen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Game 1")
 
 def main():
     run = True
@@ -10,6 +11,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+        
+        #GScreen.fill((255,255,255))
+        GScreen.fill((255,0,255))
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
