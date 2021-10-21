86a909e6f7d5028f0c8b9d53339059bb1703c6a5
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-21 23:44:36 +0700

    Import Images and Music

--- IntroductionPygame/PyInt.py
+++ IntroductionPygame/PyInt.py
@@ -1,17 +1,26 @@
 import pygame
+import os
+
 width, height = 800, 600
 
 FPS = 60
 
 GScreen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Game 1")
+PICTURE = pygame.image.load(os.path.join("D:/18110168_GITHUBS/PythonRealWorldGames/IntroductionPygame/resources","OhYouthKarryon.jpg"))
+SONG = os.path.join(os.getcwd(), "D:/18110168_GITHUBS/PythonRealWorldGames/IntroductionPygame/resources/Reminiscence-JohannesBornlof.mp3")
 
 def draw_fn():
     #GScreen.fill((255,255,255))
     GScreen.fill((255,0,255))
+    GScreen.blit(PICTURE,(0,0))
+
     pygame.display.update()
 
 def main():
+    pygame.mixer.init()
+    pygame.mixer.music.load(SONG)
+    pygame.mixer.music.play(-1)
     run = True
     clock = pygame.time.Clock()
 
