54e2357ef769a5a5bc1e7cb30c0102f5280ffc75
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-10-21 23:02:58 +0700

    Set screen FPS

--- IntroductionPygame/PyInt.py
+++ IntroductionPygame/PyInt.py
@@ -1,20 +1,27 @@
 import pygame
 width, height = 800, 600
 
+FPS = 60
+
 GScreen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Game 1")
 
+def draw_fn():
+    #GScreen.fill((255,255,255))
+    GScreen.fill((255,0,255))
+    pygame.display.update()
+
 def main():
     run = True
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-        
-        #GScreen.fill((255,255,255))
-        GScreen.fill((255,0,255))
-        pygame.display.update()
+
+        draw_fn()
 
 if __name__ == "__main__":
     main()
